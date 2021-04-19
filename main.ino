cb0197d2f25aab6f803aedc41fadae1b168eb3c0
Jae Gwan Park <47578882+thejammerr@users.noreply.github.com>
2021-04-19 10:42:32 -0400

    complete piezo/password mechanisms
    remaining todo: 
    - make chime for piezo
    - make distance LED blink
    - make IR remote functionality

--- main.ino
+++ main.ino
@@ -26,8 +26,8 @@ const int DISTANCELIGHT = 13;
 const int LDR = A0;
 
 // Helper variables
-bool objectDetected = false;
-bool doorOpen = false;
+bool objectDetected = false; // if the object is detected
+bool doorClosed = false; // if the door is opened
 int homeStatus = 0; // status of the securty system
 // 0 = OFF, 1 = HOME, 2 = AWAY
 // if the user is currently entering a password
@@ -35,7 +35,11 @@ bool enteringPassword = false;
 // if the alarm should be on
 bool soundAlarm = false;
 String userInput = ""; // users password attempt
-String correctPassword = "34"; // correct password
+String correctPassword = "01"; // correct password
+// if the button has been pressed
+int pressButton = 0;
+bool pressed3 = false, pressed4 = false;	// booleans that help with DIP3 and DIP4 input
+bool unlocked = false;	// house is unlocked after password is input correctly
 
 // LCD
 LiquidCrystal lcd(16, 17, 18, 19, 15, 2); // lcd display object
@@ -46,10 +50,12 @@ unsigned long curTime, prevTime;
 
 // This method resets everything
 void reset(){
- userInput = "";
- soundAlarm = false;
- enteringPassword = false;
- objectDetected = false;
+   	userInput = "";
+   	soundAlarm = false;
+   	enteringPassword = false;
+   	objectDetected = false;
+  	pressed3 = false;
+  	pressed4 = false;
 }
 
 // This method reads input from the slide switch 
@@ -80,26 +86,41 @@ void setStatus(){
   } else {
     lcd.print("Off");
   }
-  
 }
 
 
-// This method checks the door
+// This method checks to see if the door is open
 void checkDoor(){
   doorClosed = digitalRead(SWITCH);	// switch input: 0 is right (open), 1 is left (closed)
   
-  if (!doorClosed){		// if door is open and status is set to away
-    
-    
+  if (!doorClosed && (homeStatus == 2|| homeStatus == 1) && !unlocked){		// if door is open and status is set to away
+    lcd.clear();
+    enteringPassword = true;
+    prevTime = curTime;
+    soundAlarm = true;
+  }else if (doorClosed){
+    unlocked = false;
   }
 }
 
+// This method rings the door bell and reads the
+// button input.
 void soundBell(){
-  	
-	
+  // checks to see if the button has been pressed
+  pressButton = digitalRead(BUTTON);
+  
+  // if the button was pressed
+  if (pressButton == 1){
+    // ring the door bell for 1 second
+    digitalWrite (ALARM, HIGH);
+    delay (1000);
+  } else{ 
+    digitalWrite (ALARM, LOW);
+  }	
 }
 
 
+// This method sets the alarm based on the current state of soundAlarm
 void setAlarm(){
   if (soundAlarm){
     digitalWrite(ALARM, HIGH);
@@ -107,6 +128,7 @@ void setAlarm(){
    	digitalWrite(ALARM, LOW); 
   }
 }
+
 
 
 void pingDistance(){
@@ -124,7 +146,7 @@ void pingDistance(){
   	distance = pulseIn(DISTANCESENSOR, HIGH);
   	converted = distance /29 /2;
   	
-  	Serial.println(converted); 		// TODO: REMOVE IN FINAL
+  	//Serial.println(converted); 		// TODO: REMOVE IN FINAL
   
   	// Check if there is object in distance range
   	if (100 <= converted && converted <= 200){
@@ -140,14 +162,55 @@ void setLamp(){
 	//int brightness = map(meterReading, 0, 1023, 0, 255);
 	int ldrReading = analogRead(LDR);
 	
-  	Serial.println(ldrReading); 	// TODO: REMOVE IN FINAL
+  	//Serial.println(ldrReading); 	// TODO: REMOVE IN FINAL
 	
 	// check for darkness, and show light
-	if (ldrReading <= 300){ // TODO: CHECK THRESHOLD VALUE
+	if (ldrReading <= 400){ // TODO: CHECK THRESHOLD VALUE
     	digitalWrite(DOORLAMP, HIGH);
     }else{
       	analogWrite(DOORLAMP, LOW);
     }
+}
+
+void getPassword(){
+ 	lcd.setCursor(0, 0);
+  	lcd.print("Enter Password:");
+  	bool tempDIP3, tempDIP4;
+  	
+    // input mapping
+    tempDIP3 = digitalRead(DIP3);
+    tempDIP4 = digitalRead(DIP4);
+      	
+	if (tempDIP3 && !pressed3){
+		userInput = userInput + "0";
+      	pressed3 = true;
+      	
+    }else if (tempDIP4 && !pressed4){
+		userInput = userInput + "1";
+      	pressed4 = true;
+    }
+    
+  	//Serial.println(userInput);
+  	//Serial.print(prevTime);
+  	//Serial.print(" is prev and cur is:");
+  	//Serial.println(curTime);
+  
+  	if (userInput.equals(correctPassword)){
+      	lcd.clear();
+		soundAlarm = false;
+      	unlocked = true;
+      	setAlarm();
+      	lcd.print("Correct");
+      	delay(1000);
+      	reset();
+  	}else if (curTime - prevTime >= 5000){
+    	lcd.clear();
+    	lcd.print("Incorrect");
+      	delay(6000);
+      	soundAlarm = false;
+      	unlocked = true;
+      	reset();
+  	}
 }
 
 void setup()
@@ -155,15 +218,27 @@ void setup()
     Serial.begin(9600); // serial for debugging
  	pinMode(DIP1, INPUT);
  	pinMode(DIP2, INPUT);
+  	pinMode(DIP3, INPUT);
+  	pinMode(DIP4, INPUT);
   	lcd.begin (16,2);
+  	// initialize the LED as an output:
+    pinMode(BUTTON, INPUT);
+  	pinMode(ALARM, OUTPUT);
 }
 
 void loop()
 {
   	curTime = millis(); // set the time since execution
-	setStatus();
-  	setLamp();
-    //pingDistance();
-	
+  	if (enteringPassword){
+   		getPassword(); 
+    }else{
+     	setStatus();
+      	setLamp();
+      	checkDoor();
+      	pingDistance();
+    }
+	soundBell();
+	setAlarm();
+  
   	delay(100);
 }
